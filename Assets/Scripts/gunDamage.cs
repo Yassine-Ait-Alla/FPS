4fd8522020989469526d3284ac9e45f69aa662c2
Yassine AITALLA <yaitalla@e2r4p5.42.fr>
2017-01-31 10:40:20 +0100

    first commit

---
+++ Assets/Scripts/gunDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunDamage : MonoBehaviour
+{
+	public int				damage = 5;
+	public float			targetDistance;
+	public float			AllowedRange = 15;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		if (Input.GetButton("Fire1"))
+		{
+			RaycastHit		shot;
+			if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), shot))
+			{
+				targetDistance = shot.distance;
+				if (targetDistance < AllowedRange)
+				{
+					shot.transform.SendMessage("DeductPoints", damage);
+				}
+			}
+		}	
+	}
+}
---
+++ Assets/Scripts/gunFire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunFire : MonoBehaviour
+{
+	private AudioSource				son;
+
+	void Start ()
+	{
+		son = this.GetComponent<AudioSource>();
+	}
+	
+	void Update ()
+	{
+		if (Input.GetButton("Fire1"))
+		{
+			son.Play();
+			GetComponent<Animation>().Play("gunShot");
+		}	
+	}
+}
